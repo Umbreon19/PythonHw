2f44ad7735bf3159cf74c105044309ab8796f597
Umbreon19 <71563718+Umbreon19@users.noreply.github.com>
2020-09-26 17:41:18 -0400

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,8 @@
+def introuduction():
+    Name=input('What is your Name')
+    print('How are you doing?,'+Name+'')
+    Candy=input('What Candy do you like?')
+    print('Nice! I like '+Candy+' too!')
+    Cheese=input('What kind of Cheese do you like best?')
+    print('I could eat '+Cheese+' all the time!')
+    print('sorry I have to go now...')
